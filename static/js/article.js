0c2f47fb9bca01443621ec4fedcbf5c8c3277483
Michele <michele.cippone01@gmail.com>
2024-12-12 23:05:13 +0100

    Cardview implemented for articles. Further improvement might be needed for animations

---
+++ static/js/article.js
@@ -0,0 +1,65 @@
+const articlesContainer = document.querySelector('#articles');
+
+const articles = [
+    {
+        "id": 1,
+        "title": "How to create a simple web app using Flask",
+        "likes": 4500,
+        "url": "#"
+    },
+    {
+        "id": 2,
+        "title": "How to create a simple web app using Django",
+        "likes": 4500,
+        "url": "#"
+    },
+    {
+        "id": 3,
+        "title": "How to create a simple web app using FastAPI",
+        "likes": 4500,
+        "url": "#"
+    }
+];
+
+articles.forEach(article => {
+    articlesContainer.innerHTML += `
+                          <div class="article">
+                            <div class="wrapper">
+                                <div class="article-title">
+                                    <p class="open-sans-regular">${article.title}</p>
+                                </div>
+
+                                <div class="article-actions">
+                                    <div class="heart">
+                                        <div class="heart-number">
+                                            <p>${formatNumber(article.likes)}</p>
+                                        </div>
+                                        
+                                        <div class="heart-icon">
+                                            <i class="bi bi-heart-fill"></i>
+                                        </div>
+                                    </div>
+
+                                    <div class="redirect-btn">
+                                        <a href="${article.url}" target="_blank">
+                                            <i class="bi bi-box-arrow-up-right"></i>
+                                        </a>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    `
+});
+
+/**
+ * Function to format likes number
+ * @param {*} number the likes number
+ * @returns the formatted number
+ */
+function formatNumber(number) {
+    if (number >= 1000) {
+        return (number / 1000).toFixed(1) + 'k';
+    }
+    return number;
+}
